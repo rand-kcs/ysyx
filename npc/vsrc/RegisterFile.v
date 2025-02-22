55e27fb68a1f27542432fe242856a42f9070df55
Huang <2460969945@qq.com>
2025-02-22 21:16:58 +0800

    fix a bug on Zero register when difftest

--- npc/vsrc/RegisterFile.v
+++ npc/vsrc/RegisterFile.v
@@ -15,23 +15,24 @@ module RegisterFile #(ADDR_WIDTH = 1, DATA_WIDTH = 1) (
   reg [DATA_WIDTH-1:0] rf [2**ADDR_WIDTH-1:0];
   always @(posedge clk) begin
 	// solution 2 : change inside 
-    if (wen) rf[waddr] <= wdata;
+    if (wen && waddr !== 0) rf[waddr] <= wdata;
   end
 
 	// solution 1 ---> what's the circuit generated???
 	//assign rf[0] = 32'b0;
 
 	// solution 3, change reg0, but output from mux , price: two mux and 1 bitor
-	wire zeroFlag;
-	assign zeroFlag = |rs1;
-	MuxKey #(2, 1, 32) m0 (src1, zeroFlag, {
-		1'b0 , 32'b0,
-		1'b1 , rf[rs1]
-	});
-	MuxKey #(2, 1, 32) m1 (src2, zeroFlag, {
-		1'b0 , 32'b0,
-		1'b1 , rf[rs2]
-	});
+  // !!! NOT WORK WHEN difftest !!!
+	//wire zeroFlag;
+	//assign zeroFlag = |rs1;
+	//MuxKey #(2, 1, 32) m0 (src1, zeroFlag, {
+	//	1'b0 , 32'b0,
+	//	1'b1 , rf[rs1]
+	//});
+	//MuxKey #(2, 1, 32) m1 (src2, zeroFlag, {
+	//	1'b0 , 32'b0,
+	//	1'b1 , rf[rs2]
+	//});
 
   // For Debug:
 	assign dbg_rf = rf;	
