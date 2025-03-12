cce16d69eb7502020cf62012e5618e07b2fd5e1b
Huang <2460969945@qq.com>
2025-03-12 11:24:52 +0800

    implement malloc in klibl

--- abstract-machine/klib/src/stdlib.c
+++ abstract-machine/klib/src/stdlib.c
@@ -29,14 +29,27 @@ int atoi(const char* nptr) {
   return x;
 }
 
+static void* current_heap;
+static bool heap_init = false;
 void *malloc(size_t size) {
   // On native, malloc() will be called during initializaion of C runtime.
   // Therefore do not call panic() here, else it will yield a dead recursion:
   //   panic() -> putchar() -> (glibc) -> malloc() -> panic()
 #if !(defined(__ISA_NATIVE__) && defined(__NATIVE_USE_KLIB__))
-  panic("Not implemented");
+  //panic("Not implemented");
 #endif
-  return NULL;
+  if(!heap_init) current_heap = heap.start;
+
+
+  int align = 1;
+  for(; align <= size; align*=2); 
+  align /= 2;
+  while (align && (uint32_t)current_heap % align) {
+    current_heap++;
+  }
+  void* my_alloc = current_heap; 
+  current_heap += size;
+  return my_alloc;
 }
 
 void free(void *ptr) {
