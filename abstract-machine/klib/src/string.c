2e73a84544cb0bf204f6baec9b9afd9b77e41fd2
Huang <2460969945@qq.com>
2025-03-15 19:59:37 +0800

    implement memove

--- abstract-machine/klib/src/string.c
+++ abstract-machine/klib/src/string.c
@@ -57,7 +57,11 @@ void *memset(void *s, int c, size_t n) {
 }
 
 void *memmove(void *dst, const void *src, size_t n) {
-  panic("Not implemented");
+  void* tmp = malloc(n + 4);
+  memcpy(tmp, src, n);
+  memcpy(dst, tmp, n);
+  free(tmp);
+  return dst;
 }
 
 void *memcpy(void *out, const void *in, size_t n) {
