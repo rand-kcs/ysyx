1e4b3ecf43fc8f71bdbeb0a1d3c6575b54670e0f
Huang <2460969945@qq.com>
2025-04-13 16:17:42 +0800

    update for system inst

--- npc/vsrc/top.v
+++ npc/vsrc/top.v
@@ -57,26 +57,36 @@ wire jen; // Jump Enable
 wire [11:0] csr_addr;
 wire csr_wen;
 wire [31:0] csr_out;
+wire [31:0] csr_wdata;
+
+wire is_ecall;
+wire is_mret;
 
 
 assign snpc = pc + 4;
 
+wire [1:0] dnpc_select;
+assign dnpc_select = {is_ecall|is_mret, ben|jen};
 
-MuxKeyWithDefault #(2, 1, 32) dnpcMKWD(dnpc, ben | jen, 32'b0, {
-	1'b0, snpc,
-	1'b1, aluout
+MuxKeyWithDefault #(4, 2, 32) dnpcMKWD(dnpc, dnpc_select, 32'b0, {
+	2'b00, snpc,
+	2'b01, aluout,
+	2'b10, csr_out,
+	2'b11, csr_out
 });
+
 PC_reg pc_reg(clk, rst, dnpc, pc);
 
 // Decode Unit  -- RF -- EX   connected
 
-IDU idu (inst, rs1, rs2, rd, imm, wen, func3, funcEU, amux1, amux2, opcode, valid, mem_wen, wmask, csr_addr, csr_wen);
+IDU idu (inst, rs1, rs2, rd, imm, wen, func3, funcEU, amux1, amux2, opcode, valid, mem_wen, wmask, csr_addr, csr_wen, is_ecall, is_mret);
 assign jen = (opcode === 7'b1101111 | opcode === 7'b1100111); // jal and jalr
 
-MuxKeyWithDefault #(3, 7, 32) wdataMKWD(wdata, opcode, aluout, {
+MuxKeyWithDefault #(4, 7, 32) wdataMKWD(wdata, opcode, aluout, {
 	7'b1101111, snpc,     //jal 
 	7'b1100111, snpc,     //jalr 
-  7'b0000011, rdata_w  // lw, lh, lb, ...
+  7'b0000011, rdata_w,  // lw, lh, lb, ...
+  7'b1110011, csr_out  // csr..
 
   // unspecify opcode lead to default -- aluout
   // ( the inst with wen)
@@ -85,7 +95,9 @@ MuxKeyWithDefault #(3, 7, 32) wdataMKWD(wdata, opcode, aluout, {
 RegisterFile #(5, 32) RF(clk, wdata, rd, wen, rs1, rs2, src1, src2, rf_dbg);
 
 ExecuteUnit eu(src1, src2, imm, pc, funcEU, amux1, amux2, aluout);
-CSRs #(12, 32) csrs(clk, aluout, csr_addr, csr_wen, csr_out);
+
+CSRs #(12, 32) csrs(clk, rst, csr_wdata, csr_addr, csr_wen, is_ecall, is_mret, pc, csr_out);
+CSR_ALU csr_alu(func3,  csr_out, src1, csr_wdata);
 
 BranchUnit be(src1, src2, func3, opcode, ben);
 
