5f16e6f353ac243ad5e5b725f3805f8ac3d98a64
Huang <2460969945@qq.com>
2025-04-13 16:17:01 +0800

    add some system relative inst

--- npc/vsrc/TypeIndicator.v
+++ npc/vsrc/TypeIndicator.v
@@ -3,7 +3,7 @@ module TypeIndicator (
 	output [2:0] itype
 );
 
-MuxKeyWithDefault #(9, 7, 3) mkwd(itype, opcode, `NULL_TYPE, {
+MuxKeyWithDefault #(10, 7, 3) mkwd(itype, opcode, `NULL_TYPE, {
 	7'b0110011, `R_TYPE,    // add, sub..
 	7'b0010011, `I_TYPE,    // imm, addi..
 	7'b1100111, `I_TYPE,    // Jalr
@@ -12,7 +12,9 @@ MuxKeyWithDefault #(9, 7, 3) mkwd(itype, opcode, `NULL_TYPE, {
 	7'b0010111, `U_TYPE,    // auipc
 	7'b1101111, `J_TYPE,    // jal
   7'b0100011, `S_TYPE,     // sw, sh, sb
-  7'b1100011, `B_TYPE     // sw, sh, sb
+  7'b1100011, `B_TYPE,    // sw, sh, sb
+
+  7'b1110011, `I_TYPE     // csr.. mret, ebreak
 });
 
 endmodule
