c2575e3b0001494ea6512196bd556adc1effe8e6
Huang <2460969945@qq.com>
2025-04-13 16:17:27 +0800

    update to CSR_UNIT, fit for more op

--- npc/vsrc/CSRs.v
+++ npc/vsrc/CSRs.v
@@ -1,8 +1,13 @@
 module CSRs #(ADDR_WIDTH = 1, DATA_WIDTH = 1) (
   input clk,
+  input rst,
   input [DATA_WIDTH-1:0] wdata,
   input [ADDR_WIDTH-1:0] addr,
+  //wen only stands for csrxx inst;
   input wen,
+  input is_ecall,
+  input is_mret,
+  input [DATA_WIDTH-1:0] pc,
 
   output [DATA_WIDTH-1:0] data
 );
@@ -21,26 +26,44 @@ module CSRs #(ADDR_WIDTH = 1, DATA_WIDTH = 1) (
 
 reg [DATA_WIDTH-1:0] mcause;
 reg [DATA_WIDTH-1:0] mstatus;
+
 reg [DATA_WIDTH-1:0] mepc;
 reg [DATA_WIDTH-1:0] mtvec;
 
+wire [DATA_WIDTH-1:0] csr_data;
+
 always@(posedge clk) begin
+  if (rst) mstatus <= 32'h1800;
+  if(is_ecall) begin
+    mepc <= pc;
+    mcause  <= 32'hb;
+  end
   if(wen) begin
     case(addr)
       12'h300: mstatus <= wdata; 
       12'h305: mtvec <= wdata; 
       12'h341: mepc <= wdata; 
       12'h342: mcause <= wdata;
-      default: 
+      default: ;
     endcase
   end
 end
 
-MuxKeyWithDefault #(4, 12, 32) data_Mux(data, addr, 32'b0, {
+
+MuxKeyWithDefault #(4, 12, 32) csr_data_Mux(csr_data, addr, 32'b0, {
   {12'h300} , mstatus,
   {12'h305} , mtvec ,
   {12'h341} , mepc ,
   {12'h342} , mcause 
 });
+
+wire [2:0] select;
+assign select = {1'b0, is_ecall, is_mret};
+
+MuxKeyWithDefault #(2, 3, 32) data_Mux(data, select, csr_data, {
+  {3'b010}, mtvec,
+  {3'b001}, mepc
+});
+
 
 endmodule
