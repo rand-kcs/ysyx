6e5136978a4042e576878e542964052c2fbcb127
Huang <2460969945@qq.com>
2025-04-18 15:00:31 +0800

    init on npc CTE

--- abstract-machine/am/src/riscv/npc/cte.c
+++ abstract-machine/am/src/riscv/npc/cte.c
@@ -1,13 +1,28 @@
 #include <am.h>
 #include <riscv/riscv.h>
 #include <klib.h>
+#include <stdint.h>
+#include <sys/types.h>
 
 static Context* (*user_handler)(Event, Context*) = NULL;
 
 Context* __am_irq_handle(Context *c) {
+  // for(int i = 0; i < 32; i++)
+  //   printf("GPR[%d]: 0x%08x\n", i, c->gpr[i]);
+
   if (user_handler) {
     Event ev = {0};
     switch (c->mcause) {
+      case 0xb:
+        #ifdef __riscv_e
+        if(c->gpr[15] == -1){
+        #else
+        if(c->gpr[17] == -1){
+        #endif
+          c->mepc += 4;
+          ev.event = EVENT_YIELD;
+          break;
+        }
       default: ev.event = EVENT_ERROR; break;
     }
 
@@ -30,8 +45,14 @@ bool cte_init(Context*(*handler)(Event, Context*)) {
   return true;
 }
 
+#define XLEN 4
+#define CONTEXT_SIZE  ((NR_REGS + 3) * XLEN)
 Context *kcontext(Area kstack, void (*entry)(void *), void *arg) {
-  return NULL;
+  Context* context = (Context*) (kstack.end - CONTEXT_SIZE);
+  context->gpr[2] = (uintptr_t) context;
+  context->gpr[10] = (uintptr_t) arg;
+  context->mepc = (uintptr_t)entry;
+  return  context;
 }
 
 void yield() {
