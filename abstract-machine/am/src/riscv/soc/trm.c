e9fca136103b210a9d04fd193b95b710584921df
Huang <2460969945@qq.com>
2026-02-21 18:05:02 +0800

    fix new heap alloc to psram

--- abstract-machine/am/src/riscv/soc/trm.c
+++ abstract-machine/am/src/riscv/soc/trm.c
@@ -11,13 +11,17 @@ int main(const char *args);
 
 extern char _mrom_start;
 #define MROM_SIZE (1 * 16 * 16* 16)
-#define MROM_END  ((uintptr_t)&_mrom_start + MROM_SIZE)
+#define MROM_END  ((uintptr_t)&_mrom_start + MROM_SIZE  -1 )
 
 extern char _sram_start[];
 #define SRAM_SIZE (0x2000)
-#define SRAM_END (_sram_start + SRAM_SIZE)
+#define SRAM_END (_sram_start + SRAM_SIZE - 1)
 
-Area heap = RANGE(&_heap_start, SRAM_END);
+extern char _psram_start[];
+#define PSRAM_SIZE (0x400000)
+#define PSRAM_END (_psram_start + PSRAM_SIZE - 1)
+
+Area heap = RANGE(&_heap_start, PSRAM_END);
 #ifndef MAINARGS
 #define MAINARGS ""
 #endif
