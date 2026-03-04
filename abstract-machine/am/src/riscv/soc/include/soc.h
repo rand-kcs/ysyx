0591c4b8ab6d1a39b5f358dbeaecb812b1651a52
Huang <2460969945@qq.com>
2026-03-04 16:43:39 +0800

    soc clock

--- abstract-machine/am/src/riscv/soc/include/soc.h
+++ abstract-machine/am/src/riscv/soc/include/soc.h
@@ -5,7 +5,7 @@
 #define SERIAL_PORT     (0x10000000)
 #define UART_LSR        (0x10000005)
 #define KBD_ADDR        (0x10011000)
-#define RTC_ADDR        (DEVICE_BASE + 0x0000048)
+#define RTC_ADDR        (0x02000000)
 
 #define VGACTL_ADDR     (DEVICE_BASE + 0x0000100)
 #define AUDIO_ADDR      (DEVICE_BASE + 0x0000200)
